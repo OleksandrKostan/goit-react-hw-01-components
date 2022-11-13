2f413b66708212796c60c999010e237587040afc
Oleksandr Kostan <kostan.law@gmail.com>
2022-11-13 14:25:40 +0200

    fixs

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// завдання 1
 import { Profile } from 'components/Profile/Profile.jsx';
 import user from 'components/Profile/user.json';
 
+// завдання 2
 import { Statistics } from 'components/Statistics/Statistics.jsx';
 import data from 'components/Statistics/data.json';
 
+// завдання 3
 import { FriendList } from 'components/Friend-list/FriendList.jsx';
 import friends from 'components/Friend-list/friends.json';
 
+// завдання 4
 import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory.jsx';
 import transactions from 'components/TransactionHistory/transactions.json';
 
